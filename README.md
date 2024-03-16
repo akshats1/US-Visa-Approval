8045fc11910ecc33ad28b9af99f9b2c9fff1d563
akshats1 <akshats063@gmail.com>
2024-03-16 09:34:15 +0530

    Updated code after creaating enviornment

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # US-Visa-Approval
+
+## How to Run
+```bash
+conda create -n visa python=3.8 -y
+conda activate visa
+pip install -r requirements.txt
+python app.py
+
+```
+
+
+
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Akshat",
+    author_email="akshats063@gmail.com",
+    packages=find_packages()
+)
